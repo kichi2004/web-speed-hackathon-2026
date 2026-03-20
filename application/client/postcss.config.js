3e5d99d979ddc54513deeb2583380965e98932c7
Takayuki Ueno <mail@kichi2004.jp>
2026-03-20 18:14:12 +0900

    Tailwind CSS v4.2.2 を導入し、pnpm-lock.yaml と package.json を更新

--- application/client/postcss.config.js
+++ application/client/postcss.config.js
@@ -1,11 +1,7 @@
-const postcssImport = require("postcss-import");
-const postcssPresetEnv = require("postcss-preset-env");
+const tailwindcss = require("@tailwindcss/postcss");
 
 module.exports = {
   plugins: [
-    postcssImport(),
-    postcssPresetEnv({
-      stage: 3,
-    }),
+    tailwindcss(),
   ],
 };
